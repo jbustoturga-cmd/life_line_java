2dedaf33f0c2c1de35a22b343e3b9176fb0ac2b7
jbustoturga-cmd <jbustoturga@gmail.com>
2025-12-13 22:57:22 -0500

    configuracion de calendariocontroller

--- src/main/java/com/example/lifeline/controller/calendarioController.java
+++ src/main/java/com/example/lifeline/controller/calendarioController.java
@@ -8,23 +8,24 @@ public class calendarioController {
       
     @GetMapping("/calendario")
     public String mostrarcalendario() {
-        // Retorna el nombre del archivo HTML (ubicado en /templates/calendario.html)
+        // Retorna el nombre del archivo HTML (ubicado en /templates/calendario.html).
         return "calendario";
     }
       @GetMapping("/alarma")
     public String mostraralarma() {
-        // Retorna el nombre del archivo HTML (ubicado en /templates/calendario.html)
+        // Retorna el nombre del archivo HTML (ubicado en /templates/calendario.html).
         return "alarma";
     }
     
     @GetMapping("/nutricion")
     public String mostrarnutricion() {
-        // Retorna el nombre del archivo HTML (ubicado en /templates/nutricion.html)
+        // Retorna el nombre del archivo HTML (ubicado en /templates/nutricion.html).
         return "nutricion";
     }
      @GetMapping("/ejercicio")
     public String mostrarejercicio() {
-        // Retorna el nombre del archivo HTML (ubicado en /templates/nutricion.html)
+        // Retorna el nombre del archivo HTML (ubicado en /templates/nutricion.html).
         return "ejercicio";
     }
 }
+//
